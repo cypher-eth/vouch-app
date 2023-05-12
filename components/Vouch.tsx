e7690df1e769ce69dcf3f758e816b97503493641
cypher <decypher@skiff.com>
2023-05-12 04:13:55 -0700

    Added txn to vouch

--- components/Vouch.tsx
+++ components/Vouch.tsx
@@ -1,21 +1,38 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 import { toastError } from "@/lib/toaster"
 import { useVouchForAddy } from "@/lib/vouch"
 import Button from "@/components/Button"
 import Input from "@/components/Input"
+import useWaitForTx from "@/lib/useWaitForTx"
 
 import { ForceConnectButton } from "./Register"
 
 function Vouch({ billCode }: { billCode: string }) {
+
+  const { waitForTx } = useWaitForTx({
+    successMessage: "Vouch sent!",
+    errorMessage: "Oops, something went wrong.",
+  })
+
   const [addy, setAddy] = useState<string>()
-  const { write: vouch } = useVouchForAddy(addy!, "Good vibes at Zuzalu")
+  const { 
+    write: vouch,
+    data: txReceipt,
+    isLoading,
+  } = useVouchForAddy(addy!, "Good vibes at Zuzalu")
 
   function handleVouch() {
     if (!addy?.length) return toastError("Address cannot be empty")
     if (!vouch) return toastError("Input not valid")
     vouch()
   }
+
+  useEffect(() => {
+    if (isLoading) setAddy(undefined)
+  }, [isLoading])
+
+  waitForTx(txReceipt)
 
   if (billCode) {
     return (
@@ -33,7 +50,7 @@ function Vouch({ billCode }: { billCode: string }) {
           placeholder="Self issued identity"
         />
         <ForceConnectButton>
-          <Button onClick={handleVouch}>VOUCH</Button>
+          <Button disabled={isLoading} onClick={handleVouch}>VOUCH</Button>
         </ForceConnectButton>
       </section>
     )
